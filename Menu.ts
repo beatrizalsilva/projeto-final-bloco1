89a75ef4acdcd4c07fcb5228c8c013da8b62cf41
Beatriz <alvesbeatriz148@gmail.com>
2024-04-03 11:29:07 -0300

    feat: criação do menu

---
+++ Menu.ts
@@ -0,0 +1,67 @@
+import readlinesync = require("readline-sync")
+
+export function main() {
+    while(true) {
+        console.log("*****************************************************")
+        console.log("                                                     ")
+        console.log("                  Livraria das Memórias              ")
+        console.log("                                                     ")
+        console.log("*****************************************************")
+        console.log("                                                     ")
+        console.log("          1 - Listar todos os livros                 ")
+        console.log("          2 - Listar livro por Id                    ")
+        console.log("          3 - Cadastrar novo livro                   ")
+        console.log("          4 - Atualizar informções do livro          ")
+        console.log("          5 - Deletar livro do acervo                ")
+        console.log("          6 - Sair                                   ")
+        console.log("                                                     ")
+        console.log("*****************************************************")
+
+        console.log("Entre com a opção desejada: ")
+        let opcao: number = readlinesync.questionInt("")
+
+        if(opcao == 6) {
+            console.log("\nLivraria das Memórias - Cada página, uma memória. Cada livro, uma jornada inesquecível.")
+            about()
+            console.log("")
+            process.exit(0)
+        }
+
+        switch(opcao) {
+            case 1:
+                console.log("\nListando todos os livros...")
+                keyPress()
+                break
+            case 2:
+                console.log("\nProcurando livro por Id...")
+                keyPress()
+                break
+            case 3:
+                console.log("\nCadastrando novo livro...")
+                keyPress()
+                break
+            case 4:
+                console.log("\nAtualizando informações de um livro...")
+                keyPress()
+                break
+            case 5:
+                console.log("\nRemovendo livro do acervo...")
+                keyPress()
+                break
+            default:
+                console.log("\nOpção inválida!")
+                keyPress()
+                break
+        }
+    }
+}
+
+function keyPress(): void {
+    console.log("Pressione ENTER para continuar...")
+}
+
+function about(): void {
+    console.log(`Projeto desenvolvido por: Beatriz Alves \ngithub.com/beatrizalsilva`)
+}
+
+main()
